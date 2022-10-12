f2ce6d056566845261e651ade60b227f6ef3ddba
jacey-h <clsrn@naver.com>
2022-10-12 20:15:11 +0100

    add file

---
+++ commonapi/capicxx-core-runtime/build/CMakeFiles/CommonAPI.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CommonAPI.dir/src/CommonAPI/Address.cpp.o"
+  "CMakeFiles/CommonAPI.dir/src/CommonAPI/ContainerUtils.cpp.o"
+  "CMakeFiles/CommonAPI.dir/src/CommonAPI/IniFileReader.cpp.o"
+  "CMakeFiles/CommonAPI.dir/src/CommonAPI/Logger.cpp.o"
+  "CMakeFiles/CommonAPI.dir/src/CommonAPI/LoggerImpl.cpp.o"
+  "CMakeFiles/CommonAPI.dir/src/CommonAPI/MainLoopContext.cpp.o"
+  "CMakeFiles/CommonAPI.dir/src/CommonAPI/Proxy.cpp.o"
+  "CMakeFiles/CommonAPI.dir/src/CommonAPI/ProxyManager.cpp.o"
+  "CMakeFiles/CommonAPI.dir/src/CommonAPI/Runtime.cpp.o"
+  "CMakeFiles/CommonAPI.dir/src/CommonAPI/Utils.cpp.o"
+  "libCommonAPI.pdb"
+  "libCommonAPI.so"
+  "libCommonAPI.so.3.1.12"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CommonAPI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusAddress.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusAddress.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusAddressTranslator.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusAddressTranslator.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusClientId.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusClientId.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusConnection.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusConnection.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusDaemonProxy.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusDaemonProxy.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusError.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusError.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusFactory.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusFactory.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusFreedesktopPropertiesStub.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusFreedesktopPropertiesStub.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusFunctionalHash.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusFunctionalHash.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusInputStream.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusInputStream.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusInstanceAvailabilityStatusChangedEvent.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusInstanceAvailabilityStatusChangedEvent.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusMainLoop.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusMainLoop.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusMainLoopContext.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusMainLoopContext.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusMessage.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusMessage.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusObjectManager.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusObjectManager.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusObjectManagerStub.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusObjectManagerStub.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusOutputStream.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusOutputStream.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusProxy.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusProxy.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusProxyBase.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusProxyBase.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusProxyManager.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusProxyManager.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusServiceRegistry.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusServiceRegistry.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/CommonAPI/DBus/DBusStubAdapter.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/CommonAPI/DBus/DBusStubAdapter.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/murmurhash/MurmurHash3.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/murmurhash/MurmurHash3.cpp.o"
+  "/home/pi/commonapi/capicxx-dbus-runtime/src/pugixml/pugixml.cpp" "/home/pi/commonapi/capicxx-dbus-runtime/build/CMakeFiles/CommonAPI-DBus.dir/src/pugixml/pugixml.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMMONAPI_DBUS_VERSION_MAJOR=3"
+  "COMMONAPI_DBUS_VERSION_MINOR=1"
+  "CommonAPI_DBus_EXPORTS"
+  "DBUS_DAEMON_PROXY_DEFAULT_SEND_TIMEOUT=10000"
+  "DEFAULT_SEND_TIMEOUT=5000"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/pi/commonapi/capicxx-core-runtime/include"
+  "/home/pi/commonapi/dbus-1.10.10"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/pi/commonapi/capicxx-dbus-runtime/build/libCommonAPI-DBus.so" "/home/pi/commonapi/capicxx-dbus-runtime/build/libCommonAPI-DBus.so.3.1.12"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
